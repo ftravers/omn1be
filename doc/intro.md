310a57a801cd9b85425f64e0b3d9fe1343baef3b
Fenton Travers <fenton.travers@gmail.com>
2017-01-31 14:11:19 -0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to omn1be
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/omn1be/core_test.clj
@@ -0,0 +1,7 @@
+(ns omn1be.core-test
+  (:require [clojure.test :refer :all]
+            [omn1be.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
