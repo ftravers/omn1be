d037110a4e8494ba70bc05bb198635b3a61ce02f
Fenton Travers <fenton.travers@gmail.com>
2017-07-11 12:34:11 -0700

    basic server

--- src/omn1be/core.clj
+++ src/omn1be/core.clj
@@ -1,50 +1,25 @@
 (ns omn1be.core
-  (:require [datomic.api :as d]
-            [clojure.core.async :refer [go <! timeout]]
-            [taoensso.timbre :refer [debug]]))
+  (:require
+   [om.next.server :as om]
+   [taoensso.timbre :refer [debug warn]]))
 
-(def db-url "datomic:free://127.0.0.1:4334/omn-dev")
+(def users [{:user/name "bob" :user/password "abc123"}
+            {:user/name "fenton" :user/password "passwErd"}])
 
-(def schema
-  [{:db/doc "username"
-    :db/ident :user/name
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db.install/_attribute :db.part/db}
-   {:db/doc "password"
-    :db/ident :user/password
-    :db/valueType :db.type/string
-    :db/cardinality :db.cardinality/one
-    :db.install/_attribute :db.part/db}])
+(defn valid-user [userz username password]
+  (->> userz
+       (filter #(= username (:user/name %)))
+       first
+       :user/password
+       (= password)))
 
-(def test-data [{:user/name "fenton" :user/password "passwErd"}])
+(defn reader
+  [env kee params]
+  (debug "ENV: " env ", KEY: " kee ", PARAMS: " params)
+  (let [userz (:state env)
+        username (:user/name params)
+        password (:user/password params)]
+    {:value (valid-user userz username password)}))
 
-(defn reload-dbs
-  ([]
-   (reload-dbs db-url))
-  ([db-url]
-   (d/delete-database db-url)
-   (d/create-database db-url)
-   (d/transact (d/connect db-url) schema)
-   (d/transact (d/connect db-url) test-data)))
+(def parser (om/parser {:read reader}))
 
-(defn db [] (-> db-url d/connect d/db))
-
-(defn valid-user
-  [db username password]
-  (= 1
-     (count
-      (d/q
-       '{:find [(pull ?e [:user/name])]
-         :in [$ ?username ?password]
-         :where [[?e :user/name ?username]
-                 [?e :user/password ?password]]}
-       db username password))))
-
-(def person {:name "Fenton"
-             :age 20})
-
-(defn of-age [person country]
-  (case country
-    "USA" (>= (:age person) 21)
-    "Canada" (>= (:age person) 19)))
